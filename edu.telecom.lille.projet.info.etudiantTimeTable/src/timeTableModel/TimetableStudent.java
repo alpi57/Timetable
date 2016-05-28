9d699dad21b758177d1f4f9bb79c8ef91e1f77fd
Julien Cuvelier <cuvelier.julien59@gmail.com>
2016-05-28 17:01:17 +0200

    Alignement sur le même modèle que TimetableTeacher

--- edu.telecom.lille.projet.info.etudiantTimeTable/src/timeTableModel/TimetableStudent.java
+++ edu.telecom.lille.projet.info.etudiantTimeTable/src/timeTableModel/TimetableStudent.java
@@ -16,61 +16,78 @@ import java.util.HashSet;
  */
 public class TimetableStudent extends Timetable {
 	/**
-	 * Description of the property id_turntable.
+	 * Description of the property id_timetable.
 	 */
-	private Integer id_turntable;
+	private int id_timetable;
 
 	/**
 	 * Description of the property list_booking.
 	 */
-	public HashSet<Booking> bookings = new HashSet<Booking>();
+	public HashSet<Booking> bookings;
 	
-	// Start of user code (user defined attributes for TurntableStudent)
+	// Start of user code (user defined attributes for TimetableTeacher)
 	
 	// End of user code
 
 	/**
 	 * The constructor.
 	 */
-	public TimetableStudent() {
-		// Start of user code constructor for TurntableStudent)
-		super();
+	public TimetableStudent(int last_timetable_id) {
+		this.id_timetable=last_timetable_id;
+		this.bookings = new HashSet<Booking>();
+	}
+
+	// Start of user code (user defined methods for TimetableTeacher)
+
 		// End of user code
+		/**
+		 * Returns id_timetable.
+		 * @return id_timetable 
+		 */
+		public int getId_timetable() {
+			return this.id_timetable;
+		}
+
+		/**
+		 * Sets a value to attribute id_timetable. 
+		 * @param newId_timetable 
+		 */
+		public void setId_timetable(int newId_timetable) {
+			this.id_timetable = newId_timetable;
+		}
+
+		/**
+		 * Returns list_booking.
+		 * @return list_booking 
+		 */
+		public HashSet<Booking> getbooking() {
+			return this.bookings;
+		}
+
+		/**
+		 * Sets a value to attribute list_booking. 
+		 * @param newList_booking 
+		 */
+		public void setList_booking(HashSet<Booking> newList_booking) {
+			this.bookings = newList_booking;
+		}
+		
+		
+		/**
+		 * Add a new booking object to the timetable 
+		 * @param Booking book
+		 */
+		public void addBooking(Booking book){
+			this.bookings.add(book);
+		}
+		
+		
+		/**
+		 * Remove a new booking object to the timetable 
+		 * @param Booking book
+		 */
+		public void removeBooking(Booking book){
+			this.bookings.remove(book);
+		}
+
 	}
-
-	// Start of user code (user defined methods for TurntableStudent)
-
-	// End of user code
-	/**
-	 * Returns id_turntable.
-	 * @return id_turntable 
-	 */
-	public int getId_turntable() {
-		return this.id_turntable;
-	}
-
-	/**
-	 * Sets a value to attribute id_turntable. 
-	 * @param newId_turntable 
-	 */
-	public void setId_turntable(Integer newId_turntable) {
-		this.id_turntable = newId_turntable;
-	}
-
-	/**
-	 * Returns list_booking.
-	 * @return list_booking 
-	 */
-	public HashSet<Booking> getbooking() {
-		return this.bookings;
-	}
-
-	/**
-	 * Sets a value to attribute list_booking. 
-	 * @param newList_booking 
-	 */
-	public void setList_booking(HashSet<Booking> newList_booking) {
-		this.bookings = newList_booking;
-	}
-
-}
